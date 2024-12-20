0e63c9712b5e615e59e818b57d0a605286290d5c
Crowdbotics <team@crowdbotics.com>
2024-12-20 13:27:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "road-dog-50439",
+  url: "https://road-dog-50439.botics.co",
+  api: "https://road-dog-50439.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
